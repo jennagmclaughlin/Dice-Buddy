c4c738bae0cf5f145ee99d1efdc8936db4559fa4
jengmcla <89791791+jengmcla@users.noreply.github.com>
2024-01-24 00:32:11 -0500

    Added GIF generator with Tenor API

---
+++ src/commands/misc/astarion.js
@@ -0,0 +1,28 @@
+require('dotenv');
+const { tenor_key } = process.env;
+const { SlashCommandBuilder } = require('discord.js');
+const superagent = require('superagent');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('astarion')
+        .setDescription('Grabs an Astarion GIF.'),
+        async execute (interaction) {
+            // deferring reply
+            await interaction.deferReply();
+            // however many results to generate
+            const limit = 10;
+            // choose a random number from 0-8 and show number in channel
+            let choice = Math.floor(Math.random() * limit);
+            // grabbing link for generation
+            const link = "https://tenor.googleapis.com/v2/search?q=astarion&key=" + tenor_key + "&limit=" + limit;
+            const output = await superagent.get(link).catch(error => { console.log(error) });
+
+            try {
+                // send a reply with the gif
+                await interaction.editReply({ content: output.body.results[choice].itemurl });
+            } catch (error) {
+                return await interaction.editReply({ content: `There was an error grabbing a gif...` });
+            }
+        }
+}
